3d2323a3439e40f5eb491386a2a9affde41ec0ce
unknown <qsjiang@e3-11-30.ece.ualberta.ca>
2015-02-17 22:09:58 -0700

    add midigenerator prototype

---
+++ MIDIGeneratorPrototype/software/MIDIGenerator/obj/default/hello_ucosii.d
@@ -0,0 +1,67 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../MIDIGenerator_bsp//HAL/inc/includes.h \
+  ../MIDIGenerator_bsp//HAL/inc/os_cpu.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_irq.h \
+  ../MIDIGenerator_bsp//HAL/inc/nios2.h \
+  ../MIDIGenerator_bsp//HAL/inc/alt_types.h ../MIDIGenerator_bsp/system.h \
+  ../MIDIGenerator_bsp/linker.h ../MIDIGenerator_bsp//UCOSII/inc/os_cfg.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_alarm.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h \
+  ../MIDIGenerator_bsp//HAL/inc/priv/alt_alarm.h \
+  ../MIDIGenerator_bsp//HAL/inc/alt_types.h ../MIDIGenerator_bsp/system.h \
+  ../MIDIGenerator_bsp//UCOSII/inc/ucos_ii.h \
+  ../MIDIGenerator_bsp//UCOSII/inc/os_cfg.h \
+  ../MIDIGenerator_bsp//HAL/inc/os_cpu.h \
+  ../MIDIGenerator_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_dev.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h \
+  ../MIDIGenerator_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_alarm.h \
+  ../MIDIGenerator_bsp//HAL/inc/sys/alt_warning.h
+
+../MIDIGenerator_bsp//HAL/inc/includes.h:
+
+../MIDIGenerator_bsp//HAL/inc/os_cpu.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_irq.h:
+
+../MIDIGenerator_bsp//HAL/inc/nios2.h:
+
+../MIDIGenerator_bsp//HAL/inc/alt_types.h:
+
+../MIDIGenerator_bsp/system.h:
+
+../MIDIGenerator_bsp/linker.h:
+
+../MIDIGenerator_bsp//UCOSII/inc/os_cfg.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_alarm.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h:
+
+../MIDIGenerator_bsp//HAL/inc/priv/alt_alarm.h:
+
+../MIDIGenerator_bsp//HAL/inc/alt_types.h:
+
+../MIDIGenerator_bsp/system.h:
+
+../MIDIGenerator_bsp//UCOSII/inc/ucos_ii.h:
+
+../MIDIGenerator_bsp//UCOSII/inc/os_cfg.h:
+
+../MIDIGenerator_bsp//HAL/inc/os_cpu.h:
+
+../MIDIGenerator_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_dev.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h:
+
+../MIDIGenerator_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_llist.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_alarm.h:
+
+../MIDIGenerator_bsp//HAL/inc/sys/alt_warning.h:
