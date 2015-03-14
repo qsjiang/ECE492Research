f6a08f92e01c5bc48fbe974b85807a78b9f4c727
unknown <qsjiang@e3-11-30.ece.ualberta.ca>
2015-03-14 01:35:49 -0600

    added audio test

---
+++ AudioTest/software/AudioTest/obj/default/hello_ucosii.d
@@ -0,0 +1,85 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../AudioTest_bsp//HAL/inc/includes.h ../AudioTest_bsp//HAL/inc/os_cpu.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_irq.h \
+  ../AudioTest_bsp//HAL/inc/nios2.h ../AudioTest_bsp//HAL/inc/alt_types.h \
+  ../AudioTest_bsp/system.h ../AudioTest_bsp/linker.h \
+  ../AudioTest_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../AudioTest_bsp/system.h ../AudioTest_bsp//HAL/inc/nios2.h \
+  ../AudioTest_bsp//HAL/inc/alt_types.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_irq.h \
+  ../AudioTest_bsp//UCOSII/inc/os_cfg.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_alarm.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_llist.h \
+  ../AudioTest_bsp//HAL/inc/priv/alt_alarm.h ../AudioTest_bsp/system.h \
+  ../AudioTest_bsp//UCOSII/inc/ucos_ii.h \
+  ../AudioTest_bsp//UCOSII/inc/os_cfg.h \
+  ../AudioTest_bsp//HAL/inc/os_cpu.h \
+  ../AudioTest_bsp//drivers/inc/altera_up_avalon_audio.h \
+  ../AudioTest_bsp//HAL/inc/alt_types.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_dev.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_llist.h \
+  ../AudioTest_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../AudioTest_bsp//HAL/inc/sys/alt_llist.h \
+  ../AudioTest_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h \
+  ../AudioTest_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h \
+  ../AudioTest_bsp//HAL/inc/io.h ../AudioTest_bsp//HAL/inc/alt_types.h
+
+../AudioTest_bsp//HAL/inc/includes.h:
+
+../AudioTest_bsp//HAL/inc/os_cpu.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_irq.h:
+
+../AudioTest_bsp//HAL/inc/nios2.h:
+
+../AudioTest_bsp//HAL/inc/alt_types.h:
+
+../AudioTest_bsp/system.h:
+
+../AudioTest_bsp/linker.h:
+
+../AudioTest_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../AudioTest_bsp/system.h:
+
+../AudioTest_bsp//HAL/inc/nios2.h:
+
+../AudioTest_bsp//HAL/inc/alt_types.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_irq.h:
+
+../AudioTest_bsp//UCOSII/inc/os_cfg.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_alarm.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_llist.h:
+
+../AudioTest_bsp//HAL/inc/priv/alt_alarm.h:
+
+../AudioTest_bsp/system.h:
+
+../AudioTest_bsp//UCOSII/inc/ucos_ii.h:
+
+../AudioTest_bsp//UCOSII/inc/os_cfg.h:
+
+../AudioTest_bsp//HAL/inc/os_cpu.h:
+
+../AudioTest_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../AudioTest_bsp//HAL/inc/alt_types.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_dev.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_llist.h:
+
+../AudioTest_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../AudioTest_bsp//HAL/inc/sys/alt_llist.h:
+
+../AudioTest_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h:
+
+../AudioTest_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h:
+
+../AudioTest_bsp//HAL/inc/io.h:
+
+../AudioTest_bsp//HAL/inc/alt_types.h:
---
+++ AudioTest/software/MicByPass/obj/default/hello_ucosii.d
@@ -0,0 +1,85 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../MicByPass_bsp//HAL/inc/includes.h ../MicByPass_bsp//HAL/inc/os_cpu.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_irq.h \
+  ../MicByPass_bsp//HAL/inc/nios2.h ../MicByPass_bsp//HAL/inc/alt_types.h \
+  ../MicByPass_bsp/system.h ../MicByPass_bsp/linker.h \
+  ../MicByPass_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../MicByPass_bsp/system.h ../MicByPass_bsp//HAL/inc/nios2.h \
+  ../MicByPass_bsp//HAL/inc/alt_types.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_irq.h \
+  ../MicByPass_bsp//UCOSII/inc/os_cfg.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_alarm.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_llist.h \
+  ../MicByPass_bsp//HAL/inc/priv/alt_alarm.h ../MicByPass_bsp/system.h \
+  ../MicByPass_bsp//UCOSII/inc/ucos_ii.h \
+  ../MicByPass_bsp//UCOSII/inc/os_cfg.h \
+  ../MicByPass_bsp//HAL/inc/os_cpu.h \
+  ../MicByPass_bsp//drivers/inc/altera_up_avalon_audio.h \
+  ../MicByPass_bsp//HAL/inc/alt_types.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_dev.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_llist.h \
+  ../MicByPass_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../MicByPass_bsp//HAL/inc/sys/alt_llist.h \
+  ../MicByPass_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h \
+  ../MicByPass_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h \
+  ../MicByPass_bsp//HAL/inc/io.h ../MicByPass_bsp//HAL/inc/alt_types.h
+
+../MicByPass_bsp//HAL/inc/includes.h:
+
+../MicByPass_bsp//HAL/inc/os_cpu.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_irq.h:
+
+../MicByPass_bsp//HAL/inc/nios2.h:
+
+../MicByPass_bsp//HAL/inc/alt_types.h:
+
+../MicByPass_bsp/system.h:
+
+../MicByPass_bsp/linker.h:
+
+../MicByPass_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../MicByPass_bsp/system.h:
+
+../MicByPass_bsp//HAL/inc/nios2.h:
+
+../MicByPass_bsp//HAL/inc/alt_types.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_irq.h:
+
+../MicByPass_bsp//UCOSII/inc/os_cfg.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_alarm.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_llist.h:
+
+../MicByPass_bsp//HAL/inc/priv/alt_alarm.h:
+
+../MicByPass_bsp/system.h:
+
+../MicByPass_bsp//UCOSII/inc/ucos_ii.h:
+
+../MicByPass_bsp//UCOSII/inc/os_cfg.h:
+
+../MicByPass_bsp//HAL/inc/os_cpu.h:
+
+../MicByPass_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../MicByPass_bsp//HAL/inc/alt_types.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_dev.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_llist.h:
+
+../MicByPass_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../MicByPass_bsp//HAL/inc/sys/alt_llist.h:
+
+../MicByPass_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h:
+
+../MicByPass_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h:
+
+../MicByPass_bsp//HAL/inc/io.h:
+
+../MicByPass_bsp//HAL/inc/alt_types.h:
